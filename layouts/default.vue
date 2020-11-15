6434210d4d101a3e3d6149918cb6eeb7b8cfef00
Olupot.D <olupotd3@gmail.com>
2020-11-16 00:59:32 +0300

    Initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,148 @@
+<template>
+  <v-app>
+    <v-app-bar :clipped-left="clipped" fixed app flat>
+      <v-list-item-avatar tile color="grey">
+        <v-img
+          class="white--text align-start"
+          aspect-ratio="1"
+          src="logo.png"
+        ></v-img>
+      </v-list-item-avatar>
+      <v-toolbar-title v-text="title" class="mr-15" />
+
+      <v-text-field
+        class="ml-16"
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="Search the world's best photos and images"
+      ></v-text-field>
+
+      <v-btn text class="ma-2" to="/explore">Explore</v-btn>
+
+      <v-btn text color="warning" @click="dialog = !dialog" class="ma-2">
+        <v-icon left>mdi-upload</v-icon>Submit photo
+      </v-btn>
+      <v-divider vertical />
+      <v-btn text class="ma-2" to="/login"> Login </v-btn>
+
+      <v-btn color="success" class="mr-2" to="/register">
+        <v-icon left>mdi-account-plus</v-icon>Join free
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <nuxt />
+    </v-main>
+
+    <v-footer color="purple lighten-1" class="mt-5" padless>
+      <v-row justify="center" no-gutters>
+        <v-col cols="12" class="text-center ma-5">
+          <v-btn class="mx-4 white--text" icon>
+            <v-icon size="24px"> mdi-facebook </v-icon>
+          </v-btn>
+          <v-btn class="mx-4 white--text" icon>
+            <v-icon size="24px"> mdi-twitter </v-icon>
+          </v-btn>
+          <v-btn class="mx-4 white--text" icon>
+            <v-icon size="24px"> mdi-whatsapp </v-icon>
+          </v-btn>
+          <v-btn class="mx-4 white--text" icon>
+            <v-icon size="24px"> mdi-instagram </v-icon>
+          </v-btn>
+          <v-btn class="mx-4 white--text" icon>
+            <v-icon size="24px"> mdi-linkedin </v-icon>
+          </v-btn>
+        </v-col>
+
+        <v-col class="text-center white--text" cols="3"> Splashd Inc </v-col>
+        <v-col class="text-center white--text" cols="3"> Tenants </v-col>
+        <v-col class="text-center white--text" cols="3"> Landlords </v-col>
+        <v-col class="text-center white--text" cols="3"> Others </v-col>
+
+        <v-col class="text-center white--text" cols="3"> Careers </v-col>
+        <v-col class="text-center white--text" cols="3">
+          Terms and conditions
+        </v-col>
+        <v-col class="text-center white--text" cols="3"> How to rent </v-col>
+        <v-col class="text-center white--text" cols="3"> Others </v-col>
+
+        <v-col class="text-center white--text" cols="3"> Data Security </v-col>
+        <v-col class="text-center white--text" cols="3"> Pricing </v-col>
+        <v-col class="text-center white--text" cols="3"> </v-col>
+        <v-col class="text-center white--text" cols="3"> </v-col>
+
+        <v-col class="text-center white--text" cols="3"> Cookie policy </v-col>
+        <v-col class="text-center white--text" cols="3"> Blog for inter </v-col>
+        <v-col class="text-center white--text" cols="3"> </v-col>
+        <v-col class="text-center white--text" cols="3"> </v-col>
+
+        <v-col class="text-center white--text mb-10" cols="12">
+          {{ new Date().getFullYear() }} — <strong>Splashd</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+
+    <v-dialog v-model="dialog" max-width="700">
+      <v-card>
+        <v-card-title class="headline">
+          Upload up to 6 photos or images at a time
+        </v-card-title>
+
+        <v-card-text>
+          <v-row justify="space-around">
+            <v-col v-for="i in 6" :key="i" cols="12" md="4">
+              <v-sheet class="pa-12" color="grey lighten-3">
+                Click to attach
+              </v-sheet>
+            </v-col>
+          </v-row>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn color="warning darken-1" text @click="dialog = false">
+            Cancel
+          </v-btn>
+
+          <v-btn color="green darken-1" text @click="dialog = false">
+            Upload
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-app>
+</template>
+
+<script>
+  import { mapState, mapActions } from "vuex";
+  export default {
+    data() {
+      return {
+        dialog: false,
+        clipped: false,
+        drawer: false,
+        fixed: false,
+        right: true,
+        title: "Untamed",
+        links: ["Home", "About Us", "Team", "Services", "Blog", "Contact Us"],
+      };
+    },
+    methods: {
+      async logout() {
+        await this.$auth.logout();
+        this.$router.push("/");
+      },
+      ...mapActions({
+        logoutUser: "login/logoutUser",
+      }),
+    },
+    computed: {
+      currentUser: function () {
+        return this.$auth.user.username.toUpperCase();
+      },
+    },
+  };
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,191 @@
+<template>
+  <v-layout column>
+    <v-flex xs12 sm12 md12>
+      <v-card flat>
+        <v-img
+          height="450"
+          aspect-ratio="1"
+          class="white--text align-center"
+          src="https://images.unsplash.com/photo-1437751695201-298be97a82a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+        >
+          <v-card-title class="pa-10 text-left">
+            <v-row>
+              <v-col cols="12" sm="12" md="12">
+                <div class="text-h4 mb-5">
+                  <b>Splash</b>
+                </div>
+                <div class="text-h6">
+                  The internet’s source of freely-usable images. Powered by
+                  creators everywhere.
+                </div>
+              </v-col>
+              <v-col cols="12" sm="12" md="12">
+                <v-btn large class="pa-6" color="primary" to="/how_it_works">
+                  Take the first step
+                  <v-icon>mdi-arrow-right</v-icon>
+                </v-btn>
+              </v-col>
+
+              <v-col cols="12" sm="12" md="12" class="">
+                Photo of the day
+              </v-col>
+            </v-row>
+          </v-card-title>
+        </v-img>
+      </v-card>
+
+      <v-row class="mt-16 ml-5 mr-5 mb-16">
+        <v-col v-for="(card, i) in tech" :key="i" cols="12" sm="4">
+          <v-card class="mx-auto">
+            <v-img
+              :src="card.imageURL"
+              :aspect-ratio="1"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              lazy-src="https://picsum.photos/id/11/10/6"
+            >
+              <v-card-title class="white--text">
+                <v-row>
+                  <v-col md="10"> {{ card.title }} </v-col>
+                  <v-col md="2" class="text-right">
+                    <v-icon
+                      color="white"
+                      v-show="!loading"
+                      @click="downloadImage(card.imageURL)"
+                      >mdi-download</v-icon
+                    >
+                  </v-col>
+                </v-row>
+              </v-card-title>
+            </v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      loading: false,
+
+      tech: [
+        {
+          id: 2,
+          title: "Caravella",
+          imageURL:
+            "https://images.unsplash.com/photo-1605370458667-56d9dddabbea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+          detail_link: "/agents",
+          loading: false,
+        },
+        {
+          id: 3,
+          title: "Sony House",
+          imageURL:
+            "https://images.unsplash.com/photo-1605443791607-80a259dd3c3c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+          detail_link: "/agents",
+          loading: false,
+        },
+        {
+          id: 4,
+          title: "Canon House",
+          imageURL:
+            "https://images.unsplash.com/photo-1605398920523-26470ee935f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+          detail_link: "/agents",
+          loading: false,
+        },
+        {
+          id: 5,
+          title: "Camera House",
+          imageURL:
+            "https://images.unsplash.com/photo-1523217582562-09d0def993a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+          detail_link: "/agents",
+          loading: false,
+        },
+        {
+          id: 5,
+          title: "Camera House",
+          imageURL:
+            "https://images.unsplash.com/photo-1416331108676-a22ccb276e35?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+          detail_link: "/agents",
+          loading: false,
+        },
+        {
+          id: 5,
+          title: "Camera House",
+          imageURL:
+            "https://images.unsplash.com/photo-1452626212852-811d58933cae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+          detail_link: "/agents",
+          loading: false,
+        },
+
+        {
+          id: 3,
+          title: "Sony House",
+          imageURL:
+            "https://images.unsplash.com/photo-1605365070248-299a182a2ca6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+          detail_link: "/agents",
+          loading: false,
+        },
+        {
+          id: 4,
+          title: "Canon House",
+          imageURL:
+            "https://images.unsplash.com/photo-1449844908441-8829872d2607?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+          detail_link: "/agents",
+          loading: false,
+        },
+        {
+          id: 5,
+          title: "Camera House",
+          imageURL:
+            "https://images.unsplash.com/photo-1605020800962-8c58acb6e10b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+          detail_link: "/agents",
+          loading: false,
+        },
+        {
+          id: 5,
+          title: "Camera House",
+          imageURL:
+            "https://images.unsplash.com/photo-1416331108676-a22ccb276e35?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+          detail_link: "/agents",
+          loading: false,
+        },
+        {
+          id: 5,
+          title: "Camera House",
+          imageURL:
+            "https://images.unsplash.com/photo-1452626212852-811d58933cae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+          detail_link: "/agents",
+          loading: false,
+        },
+      ],
+      loading: false,
+    }),
+
+    methods: {
+      downloadImage(url) {
+        this.loading = true;
+        this.$axios
+          .get(url, {
+            responseType: "blob",
+          })
+          .then((res) => {
+            const blob = new Blob([res.data], { type: "application/image" });
+            const link = document.createElement("a");
+            link.href = URL.createObjectURL(blob);
+
+            link.click();
+            URL.revokeObjectURL(link.href);
+          })
+          .catch((er) => {
+            console.log(er);
+          })
+          .finally(() => {
+            this.loading = false;
+          });
+      },
+    },
+  };
+</script>
